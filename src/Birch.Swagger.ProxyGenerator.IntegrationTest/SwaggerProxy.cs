904f439c69221ec8c990641ab92940432e8a3680
Shane Ray <Shane.Ray@birch.com>
2016-04-14 15:14:27 -0400

    OSI-14: Fixed build error.

--- src/Birch.Swagger.ProxyGenerator.IntegrationTest/SwaggerProxy.cs
+++ src/Birch.Swagger.ProxyGenerator.IntegrationTest/SwaggerProxy.cs
@@ -6,9 +6,9 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Birch.Swagger.ProxyGenerator;
+using Birch.Swagger.ProxyGenerator.IntegrationTest;
 
-namespace Birch.Swagger.ProxyGenerator
+namespace Birch.Swagger.ProxyGenerator.IntegrationTest
 {
     public abstract class BaseProxy
     {
@@ -157,7 +157,7 @@ namespace Birch.Swagger.ProxyGenerator
     }
 }
 // v2/swagger.json Proxy
-namespace  {
+namespace Birch.Swagger.ProxyGenerator.IntegrationTest {
     public interface IpetWebProxy
     {
         Task addPetAsync(Pet body);
