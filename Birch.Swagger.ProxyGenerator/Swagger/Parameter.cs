9b1ce3028be32bb7471f7cd384ee08dbedf400e7
Shane Ray <Shane.Ray@birch.com>
2016-11-27 15:09:01 -0500

    OSI-18: Fixed issues with nullable and default items not being handled properly.  Also shrunk filesize by removing whitespace and duplicated code.

--- Birch.Swagger.ProxyGenerator/Swagger/Parameter.cs
+++ Birch.Swagger.ProxyGenerator/Swagger/Parameter.cs
@@ -7,14 +7,16 @@ namespace Birch.Swagger.ProxyGenerator.Swagger
         public bool IsRequired { get; set; }
         public string Description { get; set; }
         public string CollectionFormat { get; set; }
+        public string DefaultValue { get; set; }
 
-        public Parameter(TypeDefinition type, ParameterIn parameterIn, bool isRequired, string description, string collectionFormat)
+        public Parameter(TypeDefinition type, ParameterIn parameterIn, bool isRequired, string description, string collectionFormat, string propDefaultValue)
         {
-            this.Type = type;
-            this.ParameterIn = parameterIn;
-            this.IsRequired = isRequired;
-            this.Description = description;
-            this.CollectionFormat = collectionFormat;
+            Type = type;
+            ParameterIn = parameterIn;
+            IsRequired = isRequired;
+            Description = description;
+            CollectionFormat = collectionFormat;
+            DefaultValue = propDefaultValue;
         }
     }
 }
